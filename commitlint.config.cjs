6065ee8e3164d12cc663d48f15b2a948a2932a65
김용민 <dydals3440@gmail.com>
2024-10-01 05:40:45 +0900

    Feat: CI/CD Setting

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
   rules: {
     'header-min-length': [2, 'always', 20],
     'header-case-start-capital': [2, 'always'],
-    'header-end-period': [2, 'always'],
   },
   plugins: [
     {
@@ -13,9 +12,6 @@ module.exports = {
             /^[A-Z]/.test(raw),
             'Commit Message Must Start With A Capital Letter',
           ];
-        },
-        'header-end-period': ({ header }) => {
-          return [/\.$/.test(header), 'Commit Message Must End With A Period'];
         },
       },
     },
